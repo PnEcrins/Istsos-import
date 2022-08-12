34acca953f18fecd47f60663006c2ac111a99040
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2022-08-12 09:51:03 +0200

    dynamicly load procedure with API and petite vue !

--- src/istsosimport/static/js/import.js
+++ src/istsosimport/static/js/import.js
@@ -1,10 +1,14 @@
-// console.log("HELLO");
-
-// const app = PetiteVue.createApp().mount();
-// console.log(app);
-
 PetiteVue.createApp({
   $delimiters: ["${", "}"],
-  test: 4,
+  currentService: null,
+  procedures: [],
+  fetchProcedure() {
+    if (this.currentService) {
+      fetch("api/" + this.currentService + "/procedures")
+        .then((r) => r.json())
+        .then((r) => (this.procedures = r));
+    } else {
+      this.procedures = [];
+    }
+  },
 }).mount("#app");
-console.log(app);
