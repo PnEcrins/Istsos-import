64ee0abf75fbbc911f2adb95e35d5a751db37b2e
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2022-10-11 15:15:39 +0200

    remove useless columns

--- src/istsosimport/admin.py
+++ src/istsosimport/admin.py
@@ -1,12 +1,12 @@
 from flask import g, redirect, url_for
-from flask_admin import Admin, expose, AdminIndexView
+from flask_admin import Admin, expose, AdminIndexView, BaseView
 from flask_admin.contrib.sqla import ModelView
 
 from istsosimport.env import db
 from istsosimport.db.models import Import
 
 
-class HomeView(AdminIndexView):
+class HomeView(BaseView):
     @expose("/")
     def index(self):
         return redirect(url_for("import.edit_view"))
@@ -15,7 +15,7 @@ class HomeView(AdminIndexView):
 admin = Admin(
     name="Istsos-import",
     template_mode="bootstrap4",
-    # url="/",
+    url="/",
     index_view=HomeView(),
 )
 
@@ -26,6 +26,12 @@ class ImportView(ModelView):
     can_view_details = True
     list_template = "import-list2.html"
     page_size = 10
+
+    @property
+    def column_list(self):
+        return ("id_import", *self.scaffold_list_columns())
+
+    column_exclude_list = ["service", "delimiter"]
 
 
 admin.add_view(ImportView(Import, db.session))
